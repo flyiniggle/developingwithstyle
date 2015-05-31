432976390633702f1269c8f10cd3b167bed3a86d
Dan Thompson <d.thompso@yahoo.com>
2015-05-31 17:17:59 -0400

    Email validation feedback

--- app/controllers/ContactController.js
+++ app/controllers/ContactController.js
@@ -10,9 +10,32 @@ Stylish.contactController = function($scope, $http) {
 	$scope.status = 0;
 	$scope.message = "";
 	$scope.contactMe = function(){
-		var successMessage = "Thanks for the message! I'm looking forward to working with you.",
+		var f = $scope.contactForm,
+			nameMessage = "Hold on there cowboy. Please enter your name.",
+			noEmailMessage = "Slow down! Please enter your email address.",
+			invalidEmailMessage = "Whoops, it looks like your email address isn't valid.",
+			invalidPhoneMessage = "Please double-check your phone number.",
+			noMessageMessage = "Please tell me what I can do to help you.",
+			successMessage = "Thanks for the message! I'm looking forward to working with you.",
 			failureMessage = "There was an error sending your message. Please reach out to me by email, telephone, or LinkedIn.";
+
 		if ($scope.reqActive){
+			return;
+		}
+		if (f.name.$error.required) {
+			$scope.message = nameMessage;
+			return;
+		} else if (f.email.$error.required) {
+			$scope.message = noEmailMessage;
+			return;
+		} else if (f.email.$invalid) {
+			$scope.message = invalidEmailMessage;
+			return;
+		} else if (!!f.telephone.$viewValue && f.telephone.$invalid) {
+			$scope.message = invalidPhoneMessage;
+			return;
+		} else if (f.message.$error.required) {
+			$scope.message = noMessageMessage;
 			return;
 		}
 		$scope.reqActive = true;
@@ -27,11 +50,10 @@ Stylish.contactController = function($scope, $http) {
 			$scope.message = failureMessage;
 		}).then(function(){
 				$scope.reqActive = false;
-				jQuery(".contactStatus").show();
 		})
 	};
 	$scope.closeFeedback = function() {
-				jQuery(".contactStatus").hide();
+		$scope.message = "";
 	}
 };
 

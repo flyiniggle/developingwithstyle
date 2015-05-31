f668d123cd0ef700c43ae204427eb880c63e978d
Dan Thompson <d.thompso@yahoo.com>
2015-05-31 12:57:24 -0400

    Connect to yahoo server, format message, send message, show feedback

--- lib/mailer.py
+++ lib/mailer.py
@@ -1,11 +1,33 @@
 import smtplib
+import logging
 import os
+from email.errors import MessageError
+from email.mime.text import MIMEText
+from email.header import Header
+
+SMTP_SERVER = 'smtp.mail.yahoo.com'
+SMTP_PORT = '587'
+SMTP_USER = "d.thompso"
+SMTP_PASSWORD = os.environ["OPENSHIFT_EMAIL_P"]
+SMTP_EMAIL = "%s@yahoo.com" % SMTP_USER
+encoding = 'utf-8'
 
 
 class Mailer(object):
     def send_mail(self, name, their_email, telephone, message):
-        smtp_obj = smtplib.SMTP(os.environ["OPENSHIFT_PYTHON_IP"], os.environ["OPENSHIFT_PYTHON_PORT"])
-        my_email = "d.thompso@yahoo.com"
-        mail_content = "From: {n}\n Address: {e}\n  Telephone: {t}\n\n{m}".format(n=name, e=their_email, t=telephone, m=message)
+        try:
+            smtp_obj = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+            smtp_obj.ehlo()
+            smtp_obj.starttls()
+            smtp_obj.ehlo()
+            smtp_obj.login(SMTP_USER, SMTP_PASSWORD)
+            their_email = str(their_email)
+            msg = "From: {n}\nAddress: {e}\nTelephone: {t}\n\n{m}".format(n=name, e=their_email, t=telephone, m=message)
+            mail = MIMEText(msg, 'plain', encoding)
+            mail['From:'] = Header(their_email, encoding)
+            mail['Subject'] = Header('Contact request', encoding)
 
-        smtp_obj.sendmail(their_email, my_email, mail_content)
+            smtp_obj.sendmail(SMTP_EMAIL, SMTP_EMAIL, mail.as_string())
+        except Exception as e:
+            logging.exception(e)
+            raise MessageError
--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,23 @@
 import os
 import cherrypy
+from email.errors import MessageError
 
 from lib import mailer
 
 
 class application(object):
 
-	@cherrypy.expose
-	def mail(self, name="", email="", telephone="Not provided.", message=""):
-		m = mailer.Mailer()
-		try:
-			m.send_mail(name, email, telephone, message)
-			return "1"
-		except:
-			return "0"
+    @cherrypy.expose
+    def mail(self, name="", email="", telephone="Not provided.", message=""):
+        m = mailer.Mailer()
+        try:
+            m.send_mail(name, email, telephone, message)
+            return "1"
+        except MessageError:
+            return "0"
 
-	@cherrypy.expose
-	def index(self):
-		resource = open(os.path.join("", "index.html"), 'r')
-		response_body = resource.read()
-		return response_body
+    @cherrypy.expose
+    def index(self):
+        resource = open(os.path.join("", "index.html"), 'r')
+        response_body = resource.read()
+        return response_body

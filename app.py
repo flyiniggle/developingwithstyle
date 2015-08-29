2967197a904be839660ee91a00e3dac8766d631b
Dan Thompson <d.thompso@yahoo.com>
2015-08-29 08:23:12 -0400

    add CORS support for email

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ import wsgi
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
-	execfile(virtualenv, dict(__file__=virtualenv))
+    execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
-	pass
+    pass
+
+
+def CORS():
+    cherrypy.response.headers["Access-Control-Allow-Origin"] = os.environ['OPENSHIFT_APP_DNS']
 
 cherrypy.config.update({"tools.staticdir.root": os.path.dirname(os.path.abspath(__file__))})
+cherrypy.tools.CORS = cherrypy.Tool('before_handler', CORS)
 cherrypy.quickstart(wsgi.application(), "/", "conf/server.conf")

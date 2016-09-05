9ebd1c805470698b5d5c50b350a06c65523b8f19
Dan Thompson <d.thompso@yahoo.com>
2016-09-05 18:01:02 -0400

    minification, woo.

--- GruntFile.js
+++ GruntFile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		shell: {
 			integrationtest: {
 				command: 'python -m py.test -s test/integrationtest.py',
@@ -16,21 +17,45 @@ module.exports = function(grunt) {
 					stdout: true,
 					failOnError: true
 				}
-
+			}
+		},
+		ngAnnotate: {
+			options: {
+				singleQuotes: false
+			},
+			app: {
+				files: [{
+					expand: true,
+					src: ['app/stylish.js', 'app/**/*.js', '!app/libraries/*', '!app/*.min.js', '!app/*.map.js'],
+					dest: 'annotated',
+					ext: '.annotated.js',
+					extDot: 'last'
+				}]
+			}
+		},
+		concat: {
+			js: {
+				src: ['./annotated/app/stylish.annotated.js', './annotated/app/directives/*.annotated.js', './annotated/app/services/*.annotated.js', './annotated/app/animations/*.annotated.js', './annotated/app/controllers/*.annotated.js'],
+				dest: 'annotated/stylish.concat.js'
+			}
+		},
+		uglify: {
+			js: {
+				src: ['./annotated/stylish.concat.js'],
+				dest: './app/stylish.min.js'
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-ng-annotate');
+
+	grunt.registerTask('mini', ['ngAnnotate', 'concat', 'uglify']);
 
 	grunt.registerTask('test', ['shell:integrationtest', 'shell:unittest']);
 	grunt.registerTask('integrationtest', ['shell:integrationtest']);
 	grunt.registerTask('unittest', ['shell:unittest']);
 
-
-	// Load the plugin that provides the "uglify" task.
-	// grunt.loadNpmTasks('grunt-contrib-uglify');
-
-	// Default task(s).
-	//grunt.registerTask('default', ['uglify']);
 };

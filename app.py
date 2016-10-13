de1570fb5961039730bb59bd3590e4e18a10468a
Dan Thompson <d.thompso@yahoo.com>
2016-10-13 13:21:01 -0400

    more logging

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import files
 virtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'], 'virtenv')
 virtualenv = os.path.join(virtenv, 'bin', 'activate_this.py')
 conf = os.path.join(files.get_root(), "conf", "server.conf")
+help('modules')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
     print virtualenv

28802c6c2dbaf5948e9b1c73904d4bde1f7d055c
Dan Thompson <d.thompso@yahoo.com>
2016-10-13 10:26:11 -0400

    move imports to after vintrualenv is started; hopefully fix openshfit

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-import cherrypy
 import os
-
 import files
+
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin', 'activate_this.py')
+conf = os.path.join(files.get_root(), "conf", "server.conf")
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+import cherrypy
 import wsgi
 
 
 def mount():
-    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-    conf = os.path.join(files.get_root(), "conf", "server.conf")
-    try:
-        execfile(virtualenv, dict(__file__=virtualenv))
-    except IOError:
-        pass
-
-
     def CORS():
         cherrypy.response.headers["Access-Control-Allow-Origin"] = os.environ['OPENSHIFT_APP_DNS']
 

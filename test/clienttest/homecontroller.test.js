b748c31e5cf7553764e8d30a3602b445b2d87e40
Dan Thompson <d.thompso@yahoo.com>
2016-11-24 14:08:39 -0500

    another test

--- test/clienttest/homecontroller.test.js
+++ test/clienttest/homecontroller.test.js
@@ -1,21 +1,42 @@
 describe('homeController', function () {
+	var $controller;
 
 	beforeEach(module('Stylish'));
 
-  var $controller;
 
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
-  }));
+	beforeEach(inject(function(_$controller_){
+		$controller = _$controller_;
+	}));
 
 
-  describe('sum', function () {
-		it('should have four words', function () {
-			var $scope = {};
-			var controller = $controller('homeController', { $scope: $scope });
-			expect($scope.someWords.length).toBe(4);
+	describe('homeController', function () {
+		  it('should have four words', function () {
+			  var $scope = {}, controller;
+
+			  controller = $controller('homeController', { $scope: $scope });
+			  expect($scope.someWords.length).toBe(4);
+		  });
+	  });
+});
+
+describe('homeController', function () {
+	var $controller;
+
+	beforeEach(module('Stylish'));
+
+
+	beforeEach(inject(function(_$controller_){
+		$controller = _$controller_;
+	}));
+
+	describe('contactController', function () {
+		it('Closing error alert should clear message', function () {
+			var $scope = {}, controller;
+
+			controller = $controller('contactController', { $scope: $scope });
+			$scope.message = "message!";
+			$scope.closeFeedback();
+			expect($scope.message).toBe("");
 		});
 	});
-
-
 });

711a0b4ae49eab36b38bbfaf9a2bf99b3a3e99a9
Dan Thompson <d.thompso@yahoo.com>
2017-11-30 10:57:18 -0500

    probably not relevant for openshift 3 anymore anyways...

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 import files
 
+if os.environ['OPENSHIFT_PYTHON_DIR']:
+    virtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'], 'virtenv')
+    virtualenv = os.path.join(virtenv, 'bin', 'activate_this.py')
 
-virtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'], 'virtenv')
-virtualenv = os.path.join(virtenv, 'bin', 'activate_this.py')
-
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+    try:
+        execfile(virtualenv, dict(__file__=virtualenv))
+    except IOError:
+        pass
 
 import cherrypy
 import wsgi
